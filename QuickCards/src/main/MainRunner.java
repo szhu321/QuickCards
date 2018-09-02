13c2a42d50dd354f41a879b826dcb3590a61eb50
Sheng Wei <Sheng Wei@LAPTOP-3IQ8AJSM>
2018-09-01 20:41:50 -0400

    added a quick GUI

--- QuickCards/src/main/MainRunner.java
+++ QuickCards/src/main/MainRunner.java
@@ -1,10 +1,34 @@
 package main;
 
-public class MainRunner
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainRunner extends Application
 {
+	public static Stage window;
+	public static Scene scene;
+	private Group root;
+	
+	//Launches the program. Launch calls the start method.
 	public static void main(String[] args)
 	{
-		
+		launch(args);
+	}
 
+	//the start of the program
+	@Override
+	public void start(Stage mainStage) throws Exception
+	{
+		window = mainStage;
+		window.setTitle("QuickCards");
+		
+		root = new Group();
+		scene = new Scene(root, 800, 600);
+		
+		
+		window.setScene(scene);
+		window.show();
 	}
 }
