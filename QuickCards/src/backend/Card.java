17e8a720bbddc4c18d1bcbf74517678ed7f5d018
Sheng Wei <Sheng Wei@LAPTOP-3IQ8AJSM>
2018-09-01 20:16:58 -0400

    Initialize

---
+++ QuickCards/src/backend/Card.java
@@ -0,0 +1,69 @@
+package backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Card
+{
+	private String name;
+	private List<String> descriptions;
+	
+	public Card()
+	{
+		this("N/A");
+	}
+	
+	public Card(String name)
+	{
+		this(name, null);
+	}
+	
+	public Card(String name, List<String> descriptions)
+	{
+		this.name = name;
+		if(descriptions == null)
+			this.descriptions = new ArrayList<String>();
+		else
+			this.descriptions = descriptions;
+	}
+	
+	public String toString()
+	{
+		String info = "";
+		for(String desc: descriptions)
+			info += desc + "\n";
+		if(info.equals(""))
+			info = "No Description";
+		return name + "\n" + info;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public List<String> getDescriptions()
+	{
+		return descriptions;
+	}
+
+	public void setDescriptions(List<String> descriptions)
+	{
+		this.descriptions = descriptions;
+	}
+	
+	public void addDescription(String str)
+	{
+		descriptions.add(str);
+	}
+	
+	public void removeDescription(int idx)
+	{
+		descriptions.remove(idx);
+	}
+}
---
+++ QuickCards/src/main/MainRunner.java
@@ -0,0 +1,10 @@
+package main;
+
+public class MainRunner
+{
+	public static void main(String[] args)
+	{
+		
+
+	}
+}
