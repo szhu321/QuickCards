7578250e8d6e53acc0917b05e3060b94a8d7c357
Bob <Bob@Bob-PC>
2018-09-07 23:53:16 -0400

    Added files foe fxml and controller for Matching Game

---
+++ QuickCards/src/fxml/MatchingGameController.java
@@ -0,0 +1,22 @@
+package fxml;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import backend.Card;
+import backend.CardSet;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import main.MainRunner;
+import saving.FileReader;
+import saving.FileSaver;
+
+public class MatchingGameController {
+
+}
